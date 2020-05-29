9fa7ea5521c14c955f98026f5e1b9084db8e567f
johny <johny@MacBook-Pro.local>
2020-05-29 18:02:59 +0300

    updating database

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-graceful-94660',
-    user : 'johny',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
